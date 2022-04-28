04fd3ed2ea7421ac5ed91a3111c930080b6c0848
unknown <adesanyamaryo@gmail.com>
2022-04-28 13:27:16 +0100

    push str.py to github

---
+++ str.py
@@ -0,0 +1,19 @@
+course = 'Python for beginners'
+print(course.upper())
+print(course.lower())
+print(course.find('H'))
+print(course.find('for'))
+print(course.count('n'))
+print(course.replace('for', '4'))
+
+x = 10
+x += 3
+x *= 3
+print(x)
+
+x = 10 + 3 * 2
+print(x)
+
+x = (10+3) * 2
+print(x)
+
